c8e93b635ef18c01778b4259a5cd1dfcd260a3f6
vipin2909 <vipinsharma2909@gmail.com>
2022-05-26 18:52:50 +0530

    added main.java file

--- src/derekTutorial/Main.java
+++ src/derekTutorial/Main.java
@@ -5,6 +5,13 @@ import java.util.*;
 
 public class Main {
 
+    class Pair {
+        int index, commonDiff;
+        public Pair(int first, int second) {
+            this.index = first;
+            this.commonDiff = second;
+        }
+    }
     BufferedReader br;
     StringTokenizer tok;
     PrintWriter out;
@@ -33,23 +40,137 @@ public class Main {
         }
         return count;
     }
-    void solve() throws IOException {
-        int n = nextInt();
-        int arr[] = new int[n];
-        int cnt=0,temp=0,temp_max=-1;
-        for(int i = 0; i < n; i++) {
-            int a = nextInt();
-            if(a==1) {
-                cnt+=1;
-                if(temp>0) temp -= 1;
-            }
-            else {
-                temp+=1;
-                if(temp>temp_max) temp_max=temp;
+
+    private static final int  z = (int)(1e6+5);
+    boolean isprime[] = new boolean[z];
+    void siv(){
+        isprime[1]=true;
+        for(int i=2;i*i<=z;i++){
+            if(!isprime[i]){
+                for(int j=i+i;j<=z;j+=i){
+                    if(!isprime[j])isprime[j]=true;
+                }
             }
         }
-        out.println(cnt+temp_max);
     }
+
+    private static final int a = (int)(1e6+10);
+    void solve() throws IOException {
+//        int n = nextInt();
+//        int[] arr = new int[n];
+//        HashSet<Integer> set = new HashSet<Integer>();
+//        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+//        HashMap<Integer, Integer> temp = new HashMap<Integer, Integer>();
+//
+//        for(int i = 0; i < n; i++) {
+//            arr[i] = nextInt();
+//           set.add(arr[i]);
+//        }
+//        int[] a = new int[a];
+//        for(int i = 0; i < n; i++) {
+//
+//            if(map.containsKey(arr[i])) {
+//                if(temp.get(arr[i]) == 0)
+//                map.put(arr[i], i-map.get(arr[i]));
+//                else
+//                    continue;
+//            }
+//            else {
+//                map.put(arr[i], i);
+//            }
+//            temp.put(arr[i], temp.getOrDefault(arr[i], -1)+1);
+//
+//        }
+//        ArrayList<Pair> ans = new ArrayList<>();
+//        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+//            int key = entry.getKey();
+//            int mapValue = entry.getValue();
+//            int tempValue = temp.get(key);
+//            if(tempValue==0) {
+//                ans.add(new Pair(key, 0));
+//            }
+//            else {
+//                ans.add(new Pair(key, mapValue));
+//            }
+//        }
+
+//        out.println(set.size());
+//        for(int i = 0; i < ans.size(); i++) {
+//            out.println(ans.get(i).first + " " + ans.get(i).second);
+//        }
+
+        int n = nextInt();
+        int arr[] = new int[n];
+        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer, Pair> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = nextInt();
+            set.add(arr[i]);
+        }
+        HashMap<Integer, Integer> temp = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            if(map.containsKey(arr[i])) {
+                int diff = map.get(arr[i]).commonDiff;
+                if(diff == 0) {
+                    int newDiff = i - map.get(arr[i]).index;
+                    map.put(arr[i], new Pair(i, newDiff));
+//                    temp.put(arr[i], newDiff);
+                }
+                else {
+                    int newDiff = i - map.get(arr[i]).index;
+                    if(newDiff == map.get(arr[i]).commonDiff) {
+                        map.put(arr[i], new Pair(i, newDiff));
+                    }
+                    else {
+                        map.remove(arr[i]);
+                        set.remove(arr[i]);
+                    }
+                }
+            }
+            else {
+                map.put(arr[i], new Pair(i, 0));
+            }
+        }
+        out.println(set.size());
+
+        List<Integer> list = new ArrayList<Integer>(set);
+        Collections.sort(list);
+         Iterator<Integer> itr = list.listIterator();
+        while(itr.hasNext()) {
+            int x = itr.next();
+            out.println(x+" "+map.get(x).commonDiff);
+        }
+        //        for(Map.Entry<Integer, Pair> entry: map.entrySet()) {
+//            out.println(entry.getKey()+" "+entry.getValue().commonDiff);
+//        }
+
+
+
+    }
+//    void solve() throws IOException {
+//       int n = nextInt();
+//       int[] arr = new int[n];
+//
+//
+//       HashMap<Integer, Integer> map = new HashMap<>();
+//       HashSet<Integer> set = new HashSet<>();
+//        for(int i = 0; i < n; i++) {
+//           arr[i] = nextInt();
+//           set.add(arr[i]);
+//           map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+//       }
+//        ArrayList<Pair> ans = new ArrayList<>();
+//
+//        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+//            ans.add(new Pair(entry.getKey(), n/entry.getValue() == n ? 0 : n / entry.getValue()));
+//        }
+//        out.println(set.size());
+//        for(int i = 0; i < ans.size(); i++) {
+//            out.println(ans.get(i).first + " " + ans.get(i).second);
+//        }
+//
+//    }
     public static void main(String[] args) {
         try {
             Main obj = new Main();
